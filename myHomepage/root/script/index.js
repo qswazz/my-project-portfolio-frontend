dfac2b6cd386205bc7bd4cfd1d24e467d4bed54c
qswazz <89292521+qswazz@users.noreply.github.com>
2021-08-28 22:57:34 +0900

    Design : Modify CSS Layout (part. Portfolio) body : 	1. Modify Test CSS Layout (Part. Portfolio)     2. Add Resource (Portfolio video)

--- myHomepage/root/script/index.js
+++ myHomepage/root/script/index.js
@@ -2,6 +2,8 @@
 const tabs = document.querySelectorAll(".item-tab");
 const tabContents = document.querySelectorAll(".item-tab-content");
 
+const CLICKED_STAT = "active";
+
 tabs.forEach(function(t)
             {
                 t.addEventListener("click", clickTabs);
@@ -13,18 +15,39 @@ function clickTabs(t)
 
     tabContents.forEach(function(tc)
     {
-        tc.classList.remove("active");
+        tc.classList.remove(CLICKED_STAT);
     });
 
     tabs.forEach(function(t)
     {
-        t.classList.remove("active");
+        t.classList.remove(CLICKED_STAT);
     });
     
     const selectedTab = this.getAttribute("href");
     const focusedTab = document.querySelector(selectedTab);
 
-    this.classList.add("active");
-    focusedTab.classList.add("active");    
+    this.classList.add(CLICKED_STAT);
+    focusedTab.classList.add(CLICKED_STAT);    
 }
 //#endregion [Tab Function]
+
+
+//#region [Hover mp4 Play]
+const vdo = document.querySelector(".content-mp4");
+
+const VIDEO_PLAY = "play";
+
+vdo.addEventListener("mouseover", PlayVideo);
+vdo.addEventListener("mouseout", PauseVideo);
+
+function PlayVideo()
+{
+    vdo.classList.add(VIDEO_PLAY);
+    vdo.play();
+}
+function PauseVideo()
+{
+    vdo.classList.remove(VIDEO_PLAY);
+    vdo.pause();
+}
+//#endregion [Hover mp4 Play]
